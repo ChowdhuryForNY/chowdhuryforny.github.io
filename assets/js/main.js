62c724b6f97bef501918d1340005997f9a7eccbb
Gillgamesh <ggill@stuy.edu>
2018-04-04 20:21:52 -0400

    :(

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,20 @@
 
   ga('create', 'UA-99272358-1', 'auto');
   ga('send', 'pageview');
+var modal = document.getElementById('mainmodal');
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 (function($) {
 
